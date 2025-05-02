d90bfe1bd53589add43b46701535cb35e4c5c1da
Gabriel Rivera <gabo.grm27@gmail.com>
2025-05-02 15:42:07 -0600

    Seccion 14

--- src/router.tsx
+++ src/router.tsx
@@ -2,16 +2,23 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import LoginView from './views/LoginView';
 import RegisterView from './views/RegisterView';
 import AuthLayout from './layouts/AuthLayout';
+import AppLayout from './layouts/AppLayout';
+import LinkTreeView from './views/LinkTreeView';
+import ProfileView from './views/ProfileView';
 
 export default function Router() {
-	return (
-		<BrowserRouter>
-			<Routes>
-				<Route element={<AuthLayout />}>
-					<Route path="/auth/login" element={<LoginView />}></Route>
-					<Route path="/auth/register" element={<RegisterView />}></Route>
-				</Route>
-			</Routes>
-		</BrowserRouter>
-	);
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route element={<AuthLayout />}>
+          <Route path="/auth/login" element={<LoginView />}></Route>
+          <Route path="/auth/register" element={<RegisterView />}></Route>
+        </Route>
+        <Route path="/admin" element={<AppLayout />}>
+          <Route index={true} element={<LinkTreeView />}></Route>
+          <Route path="profile" element={<ProfileView />}></Route>
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }

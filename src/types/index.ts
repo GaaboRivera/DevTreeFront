7af397d8a991ac48c23f676e6946828bf4645c64
Gabriel Rivera <gabo.grm27@gmail.com>
2025-05-05 14:31:25 -0600

    Seccion 15

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,14 +1,18 @@
 export type User = {
-	handle: string;
-	name: string;
-	email: string;
+  handle: string;
+  name: string;
+  email: string;
+  _id: string;
+  description: string;
 };
 
 export type RegisterForm = Pick<User, 'handle' | 'email' | 'name'> & {
-	password: string;
-	password_confirmation: string;
+  password: string;
+  password_confirmation: string;
 };
 
 export type LoginForm = Pick<User, 'email'> & {
-	password: string;
+  password: string;
 };
+
+export type ProfileForm = Pick<User, 'handle' | 'description'>;
--- src/views/ProfileView.tsx
+++ src/views/ProfileView.tsx
@@ -1,3 +1,90 @@
+import { useForm } from 'react-hook-form';
+import ErrorMessage from '../components/ErrorMessage';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { ProfileForm, User } from '../types';
+import { updateProfile } from '../api/DevTreeAPI';
+import { toast } from 'sonner';
+
 export default function ProfileView() {
-  return <div>ProfileView</div>;
+  const queryClient = useQueryClient();
+  const data: User = queryClient.getQueryData(['user'])!;
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<ProfileForm>({
+    defaultValues: {
+      handle: data.handle,
+      description: data.description,
+    },
+  });
+
+  const updateProfileMutation = useMutation({
+    mutationFn: updateProfile,
+    onError: (error) => {
+      toast.error(error.message);
+    },
+    onSuccess: (data) => {
+      toast.success(data);
+      queryClient.invalidateQueries({ queryKey: ['user'] });
+    },
+  });
+
+  const handleUserProfileForm = (formData: ProfileForm) => {
+    updateProfileMutation.mutate(formData);
+  };
+
+  return (
+    <form
+      className="p-10 space-y-5 bg-white rounded-lg"
+      onSubmit={handleSubmit(handleUserProfileForm)}
+    >
+      <legend className="text-2xl text-center text-slate-800">
+        Editar Información
+      </legend>
+      <div className="grid grid-cols-1 gap-2">
+        <label htmlFor="handle">Handle:</label>
+        <input
+          type="text"
+          className="p-2 border-none rounded-lg bg-slate-100"
+          placeholder="handle o Nombre de Usuario"
+          {...register('handle', {
+            required: 'El nombre de usuario es obligatorio',
+          })}
+        />
+        {errors.handle && <ErrorMessage>{errors.handle.message}</ErrorMessage>}
+      </div>
+
+      <div className="grid grid-cols-1 gap-2">
+        <label htmlFor="description">Descripción:</label>
+        <textarea
+          className="p-2 border-none rounded-lg bg-slate-100"
+          placeholder="Tu Descripción"
+          {...register('description')}
+        />
+        {errors.description && (
+          <ErrorMessage>{errors.description.message}</ErrorMessage>
+        )}
+      </div>
+
+      <div className="grid grid-cols-1 gap-2">
+        <label htmlFor="handle">Imagen:</label>
+        <input
+          id="image"
+          type="file"
+          name="handle"
+          className="p-2 border-none rounded-lg bg-slate-100"
+          accept="image/*"
+          onChange={() => {}}
+        />
+      </div>
+
+      <input
+        type="submit"
+        className="w-full p-2 text-lg font-bold uppercase rounded-lg cursor-pointer bg-cyan-400 text-slate-600"
+        value="Guardar Cambios"
+      />
+    </form>
+  );
 }
